0ef25c92825f57f5ea48c8666333f9efc0d8fd5c
julio <julio336@hotmail.com>
2020-06-15 17:25:20 -0500

    adding counter to automatically update

--- src/containers/BitsoApi.js
+++ src/containers/BitsoApi.js
@@ -6,13 +6,19 @@ import Row from 'react-bootstrap/Row';
 import Spinner from 'react-bootstrap/Spinner';
 import moment from 'moment';
 import Alert from 'react-bootstrap/Alert';
+import Button from 'react-bootstrap/Button';
+
+function refreshPage() {
+    window.location.reload(false);
+  }
+  
 
 function BtxItem (book) {
     return book ? (
         Object.values(book).map((book) => {
-            return(
+            return book? (
             <>
-                <Col md="4">
+                <Col id="test" md="4">
                     <Card style={{ width: '18rem' }}>
                         <Card.Body>
                             <h1>{book.book}</h1>
@@ -36,7 +42,7 @@ function BtxItem (book) {
                     </Card>
                 </Col>
             </>
-            )
+            ):null;
         })
     ):null;
 };
@@ -44,36 +50,71 @@ function BtxItem (book) {
 class BitsoApi extends React.Component{
     constructor(){
         super();
+        this.forceUpdateHandler = this.forceUpdateHandler.bind(this);
         this.state = {
             stateBooks: [],
             isLoading: true,
+            currentCount: 60
         };
     }
 
-    componentWillMount(){
-        
-        const books = [
-            "btc_mxn", "btc_usd", "tusd_mxn", "eth_mxn", "xrp_mxn", "ltc_mxn" , "mana_mxn", "gnt_mxn", "bat_mxn", "dai_mxn"
-        ];
-        const url = "https://cors-anywhere.herokuapp.com/https://api.bitso.com/v3/ticker/?book="
-        for (const [index, value] of books.entries()) {
-            const newLocal = "XMLHttpRequest";
-            axios.get(url+value, {
-                mode: 'no-cors',
-                secure: false,
-                headers:{
-                    "X-Requested-With": newLocal
-                } 
-            }).then(result => {
-                this.setState({stateBooks: this.state.stateBooks.concat([result.data.payload]),
-                isLoading: false}
-                );
-            })
+    timer() {
+        this.setState({
+            currentCount: this.state.currentCount - 1
+        })
+        console.log(this.state.currentCount);
+        if(this.state.currentCount === 1) { 
+            console.log(this.state);
+            this.state.stateBooks = [];
+            this.state.isLoading = true;
+            this.state.currentCount = 60;
+            this.intervalId = setInterval(this.timer.bind(this), 1000);
+            this.forceUpdateHandler();
+            clearInterval(this.intervalId);
+        }
+      }
 
+    forceUpdateHandler(){
+        if (this.state.isLoading){
+            const books = [
+                "btc_mxn", "btc_usd", "tusd_mxn", "eth_mxn", "xrp_mxn", "ltc_mxn" , "mana_mxn", "gnt_mxn", "bat_mxn", "dai_mxn"
+            ];
+            const url = "https://cors-anywhere.herokuapp.com/https://api-dev.bitso.com/v3/ticker/?book="
+            for (const [index, value] of books.entries()) {
+                const newLocal = "XMLHttpRequest";
+                axios.get(url+value, {
+                    mode: 'no-cors',
+                    secure: false,
+                    headers:{
+                        "X-Requested-With": newLocal
+                    } 
+                }).then(result => {
+                    this.setState({stateBooks: this.state.stateBooks.concat([result.data.payload]),
+                    isLoading: false}
+                    );
+                })
+
+            }
+        }
+      };
+
+  
+
+    componentDidMount(){
+        if (this.state.isLoading){
+            this.intervalId = setInterval(this.timer.bind(this), 1000);
+            this.forceUpdateHandler();
         }
     }
+
+    componentWillUnmount(){
+        clearInterval(this.intervalId);
+    }
+    
+    componentWillUnmount() {
+        this.state.isLoading = true;
+    }
         render() {
-            console.log(this.state.isLoading);
             if (this.state.isLoading) {
                 return(
                     <>
@@ -85,17 +126,22 @@ class BitsoApi extends React.Component{
                     </>
                 );
             }else{
-                
 
                 const currentDate =  moment().format('MMMM Do YYYY, h:mm:ss a');
                 return(
                     <>
-                        <Col md="12">
-                            <Col md={{ span: 6, offset: 3 }} sm="12">
-                                <Alert variant="success">
-                                    <Alert.Heading>Last update: {currentDate}</Alert.Heading>
-                                </Alert>
-                            </Col>
+                        <Col md={{ span: 4, offset:2}}>
+                            <Alert variant="success">
+                                <Alert.Heading>Last update: {currentDate}</Alert.Heading>
+                            </Alert>
+                        </Col>
+                            <Col md={{ span: 6}}>   
+                            <Button onClick={() => { 
+                                this.state.isLoading = true;
+                                this.state.stateBooks = [];
+                                this.forceUpdateHandler();
+                                }}>Update
+                            </Button>
                         </Col>
                         {this.state.stateBooks.map((book) => <BtxItem book={book}/>)}
                     </>
